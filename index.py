a559c4f549108b3661d4939029403895a7ac8fab
DucQuan95 <62573142+DucQuan95@users.noreply.github.com>
2021-08-12 20:29:52 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/img/<number>')
+def getImage(number):
+    file_name = './static/' + number + '.jpg' 
+    return send_file(file_name)
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000, debug=True)
